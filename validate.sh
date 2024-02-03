fa5866557c15a011840deb7563d1f0093f7fd293
~ . ~ <156969148+wandmagic@users.noreply.github.com>
2024-02-02 20:24:25 -0500

    update file-names & oscal versions

--- validate.sh
+++ validate.sh
@@ -3,8 +3,8 @@ oscal-cli catalog validate ./src/examples/catalog/xml/basic-catalog.xml
 oscal-cli ssp validate ./src/examples/ssp/xml/oscal_leveraged-example_ssp.xml
 oscal-cli ssp validate ./src/examples/ssp/xml/oscal_leveraging-example_ssp.xml
 oscal-cli ssp validate ./src/examples/ssp/xml/ssp-example.xml
-oscal-cli ap validate ./src/examples/ap/xml/assessment-plan-example-1.xml
-oscal-cli ar validate ./src/examples/ar/xml/assessment-results-example-1.xml
-oscal-cli ssp validate ./src/examples/ssp/xml/workshop-ssp-example.xml
+oscal-cli ap validate ./src/examples/ap/xml/ifa_assessment-plan-example.xml
+oscal-cli ar validate ./src/examples/ar/xml/ifa_assessment-results-example.xml
+oscal-cli ssp validate ./src/examples/ssp/xml/ifa_ssp-example.xml
 oscal-cli component-definition validate ./src/examples/component-definition/xml/example-component-definition.xml
-oscal-cli poam validate ./src/examples/poam/xml/plan-of-action-and-milestones.xml
+oscal-cli poam validate ./src/examples/poam/xml/ifa_plan-of-action-and-milestones.xml
